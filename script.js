23f9e4bc5f8a64fabbee59a2d2df11894ec23f01
Sushant Sharma <79782587+sushantsharma0221@users.noreply.github.com>
2022-06-03 20:15:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector(".btn");
+let input = document.querySelector(".text");
+let limit = document.querySelector(".limit");
+let max = 280;
+// let min = 0;
+const updateLimit = () => {
+  limit.textContent = max;
+  input.addEventListener("input", () => {
+    let inputLength = input.value.length;
+    limit.textContent = max - inputLength;
+    if (inputLength > max) {
+      btn.disabled = true;
+      btn.style.background = "gray";
+      btn.innerText = "Limit Exceded";
+      limit.style.color = "red";
+    } else {
+      btn.disabled = false;
+      limit.style.color = "black";
+    }
+  });
+};
+updateLimit();
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const tweetInput = "https://twitter.com/intent/tweet?text=";
+  window.open(`${tweetInput}${input.value}`);
+});
